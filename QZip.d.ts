c12a3aa96136d8d4d4cc57a05bda4c5f8c23bc97
Qi Zhao <justdoit11@gmail.com>
2015-11-20 12:54:38 -0600

    Add: QZip typescript definition.

---
+++ QZip.d.ts
@@ -0,0 +1,62 @@
+﻿/**************************
+ * Type definition for QZip v1.0
+ * by Qi Zhao
+ *
+ **************************/
+
+/**
+ * QZip namespace
+ */
+declare module QZip {
+
+    /**
+     * Represent a file/dir entry in zip file
+     */
+    interface ZipEntry {
+
+        /**
+         * Whether this entry is an zip 64 entry
+         */
+        zip64: boolean;
+
+        /**
+         * Last modified date of this entry
+         */
+        date: Date;
+
+        /**
+         * Uncompressed size in bytes
+         */
+        uncompressedSize: number;
+
+        /**
+         * Filename with relative path
+         */
+        fileName: string;
+
+        /**
+         * Whether this entry is a directory or file
+         */
+        isDir: boolean;
+    }
+
+    /**
+     * Represent a Zip file
+     */
+    interface ZipFile {
+
+        /**
+         * Create a zip file instance and bind onload and onerror events
+         */
+        new (file: any, onload: (entries: Array<ZipEntry>) => void, onerror: (msg: string) => void): ZipFile;
+    }
+
+    /**
+     * Initialize a zip file instance
+     */
+    var ZipFile: ZipFile;
+}
+
+declare module "QZip" {
+    export = QZip;
+}
